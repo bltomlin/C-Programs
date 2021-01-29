3d9c5369344b84d958807533adf5d2982525e732
Brendan <brendan.tomlinson2@gmail.com>
2021-01-29 05:55:21 -0800

    Update tempc.c

--- tempc.c
+++ tempc.c
@@ -15,7 +15,7 @@ main()
     printf("%-20s%20s\n", fahre, cels);
 	while ( fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
-		printf("%5.0f %32.1f\n", fahr, celsius);
+		printf("%5.0f %32.1f\n", fahr, celsius); //
 		fahr = fahr + step;
 	}
 }
